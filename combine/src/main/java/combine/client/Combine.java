097e1257f3570d63e4104991bcc920f7f7cc841b
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2021-02-03 11:07:35 +0100

    Use only pure functions in combine example

--- combine/src/main/java/combine/client/Combine.java
+++ combine/src/main/java/combine/client/Combine.java
@@ -2,7 +2,6 @@ package combine.client;
 
 import static com.intendia.rxgwt2.elemento.RxElemento.fromEvent;
 import static elemental2.dom.DomGlobal.document;
-import static io.reactivex.Observable.defer;
 import static io.reactivex.Observable.just;
 import static io.reactivex.Observable.merge;
 import static org.jboss.gwt.elemento.core.Elements.div;
@@ -17,6 +16,7 @@ import elemental2.dom.HTMLElement;
 import elemental2.dom.HTMLInputElement;
 import elemental2.dom.HTMLLabelElement;
 import io.reactivex.Observable;
+import jsinterop.base.Js;
 import org.jboss.gwt.elemento.core.Elements;
 import org.jboss.gwt.elemento.core.InputType;
 import org.jboss.gwt.elemento.core.builder.HtmlContentBuilder;
@@ -35,8 +35,8 @@ public class Combine implements EntryPoint {
                 .map(ev -> new int[] { (int) ev.clientX, (int) ev.clientY });
         Observable<int[]> touchCoordinates$ = fromEvent(document, touchmove).map(ev -> ev.touches.item(0))
                 .map(ev -> new int[] { (int) ev.clientX, (int) ev.clientY });
-        Observable<Boolean> showCoordinates$ = fromEvent(input, change).map(ev -> input.checked)
-                .startWith(defer(() -> Observable.just(input.checked)));
+        Observable<Boolean> showCoordinates$ = fromEvent(input, change).map(ev -> Js.<HTMLInputElement>cast(ev.target))
+                .mergeWith(just(input)).map(el -> el.checked);
 
         // combine them all (first line) and subscribe (second line)…
         showCoordinates$.switchMap(show -> show ? merge(mouseCoordinates$, touchCoordinates$) : just(new int[0]))
